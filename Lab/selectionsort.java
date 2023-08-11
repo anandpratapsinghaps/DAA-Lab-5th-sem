78f53c668cb25562c000198b30f1c984e774ece7
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2023-08-11 09:39:23 +0530

    last commit

--- Lab/selectionsort.java
+++ Lab/selectionsort.java
@@ -1,10 +1,10 @@
 public class selectionsort {
-    static void Selectionsort(int arr[]){
+    static void Selectionsort(int []arr){
         int size=arr.length;
         for(int i=0;i<size-1;i++){
-            int min=arr[i];
-            for(int j=i+1;j<size-1;j++){
-                if(arr[j]<min){
+            int min=i;
+            for(int j=i+1;j<size;j++){
+                if(arr[j]<arr[min]){
                     min=j;
                 }
             }
@@ -15,5 +15,9 @@ public class selectionsort {
     }
     public static void main(String[] args) {
         int [] arr={5,10,51,15,66,11};
+        Selectionsort(arr);
+        for(int i=0;i<arr.length-1;i++){
+            System.out.print(arr[i] +" ");
+        }
     }
 }
